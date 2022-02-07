566ef6a6bc5a45fc6394086773b25036619ec190
Pixelatory <nick.aksamit@hotmail.com>
2022-02-07 14:51:35 -0500

    Another commit for changes to python

--- gym_wordle/envs/wordle_env.py
+++ gym_wordle/envs/wordle_env.py
@@ -4,19 +4,40 @@ import numpy as np
 import pkg_resources
 import random
 from typing import Optional
+import colorama
+from colorama import Fore
+from colorama import Style
+
+from gym_wordle.exceptions import InvalidWordException
+
+colorama.init(autoreset=True)
 
 # global game variables
 GAME_LENGTH = 6
 WORD_LENGTH = 5
 
-# load encoded words
+# load words and then encode
 filename = pkg_resources.resource_filename(
     'gym_wordle',
-	'data/words_encoded.txt'
+    'data/5_words.txt'
 )
+
+def encodeToStr(encoding):
+    string = ""
+    for enc in encoding:
+        string += chr(ord('a') + enc)
+    return string
+
+def strToEncode(lines):
+    encoding = []
+    for line in lines:
+        assert len(line.strip()) == 5  # Must contain 5-letter words for now
+        encoding.append(tuple(ord(char) - 97 for char in line.strip()))
+    return encoding
+
+
 with open(filename, "r") as f:
-    lines = f.readlines()
-WORDS = [tuple(int(num) for num in line.strip().split()) for line in lines]
+    WORDS = strToEncode(f.readlines())
 
 
 class WordleEnv(gym.Env):
@@ -32,7 +53,7 @@ class WordleEnv(gym.Env):
           location, the character is yellowed out (encoded as 1 in the
           environment)
         * if the character is in the hidden word and in the correct
-          location, the character is greened out (encoded as 2 in the 
+          location, the character is greened out (encoded as 2 in the
           environment)
 
     The player continues to guess until they have either guessed the correct
@@ -52,21 +73,27 @@ class WordleEnv(gym.Env):
 			based on the most recent guess.
           - alphabet: the alphabet is a length 26 Box corresponding to the guess status
             for each letter in the alaphabet. As the start, all values are -1, as no letter
-            has been used in a guess. As the player guesses words, the letters in the 
+            has been used in a guess. As the player guesses words, the letters in the
             alphabet will change to values in the range [0, 2] indicating whether the
             characters are missing in the hidden word, in the incorrect position,
             or in the correct position.
     """
 
     def __init__(self):
+        super(WordleEnv, self).__init__()
         self.action_space = spaces.MultiDiscrete([26] * WORD_LENGTH)
         self.observation_space = spaces.Dict({
             'board': spaces.Box(low=-1, high=2, shape=(GAME_LENGTH, WORD_LENGTH), dtype=int),
             'alphabet': spaces.Box(low=-1, high=2, shape=(26,), dtype=int)
         })
+        self.guesses = []
 
     def step(self, action):
         assert self.action_space.contains(action)
+
+        # Action must be a valid word
+        if not tuple(action) in WORDS:
+            raise InvalidWordException(encodeToStr(action) + " is not a valid word.")
 
         # update game board and alphabet tracking
         board_row_idx = GAME_LENGTH - self.guesses_left
@@ -84,6 +111,9 @@ class WordleEnv(gym.Env):
 
         # update guesses remaining tracker
         self.guesses_left -= 1
+
+        # update previous guesses made
+        self.guesses.append(action)
 
         # check to see if game is over
         if all(self.board[board_row_idx, :] == 2):
@@ -109,4 +139,34 @@ class WordleEnv(gym.Env):
         self.board = np.negative(
             np.ones(shape=(GAME_LENGTH, WORD_LENGTH), dtype=int))
         self.alphabet = np.negative(np.ones(shape=(26,), dtype=int))
+        self.guesses = []
         return self._get_obs()
+
+    def render(self, mode="human"):
+        assert mode in ["human"], "Invalid mode, must be \"human\""
+        print('###################################################')
+        for i in range(len(self.guesses)):
+            for j in range(WORD_LENGTH):
+                letter = chr(ord('a') + self.guesses[i][j])
+                if self.board[i][j] == 0:
+                    print(Fore.BLACK + Style.BRIGHT + letter + " ", end='')
+                elif self.board[i][j] == 1:
+                    print(Fore.YELLOW + Style.BRIGHT + letter + " ", end='')
+                elif self.board[i][j] == 2:
+                    print(Fore.GREEN + Style.BRIGHT + letter + " ", end='')
+            print()
+        print()
+
+        for i in range(len(self.alphabet)):
+            letter = chr(ord('a') + i)
+            if self.alphabet[i] == 0:
+                print(Fore.BLACK + Style.BRIGHT + letter + " ", end='')
+            elif self.alphabet[i] == 1:
+                print(Fore.YELLOW + Style.BRIGHT + letter + " ", end='')
+            elif self.alphabet[i] == 2:
+                print(Fore.GREEN + Style.BRIGHT + letter + " ", end='')
+            elif self.alphabet[i] == -1:
+                print(letter + " ", end='')
+        print()
+        print('###################################################')
+        print()
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(name='gym_wordle',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym==0.17.2',
-                        'numpy>=1.19.2'],
-      package_data={'gym_wordle': ['data/*.txt']}
+                        'numpy>=1.19.2',
+                        'colorama>=0.4.4'],
+      package_data={'gym_wordle': ['data/*.txt', 'envs/*']},
+      packages=['gym_wordle'],
 )
